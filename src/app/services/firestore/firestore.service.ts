0c14af24f8c88265c4997dd528eceb71c94f76bf
Luis Urruela <luis.urruela@gmail.com>
2023-11-13 02:22:29 -0600

    new product functions added

--- src/app/services/firestore/firestore.service.ts
+++ src/app/services/firestore/firestore.service.ts
@@ -10,7 +10,33 @@ export class FirestoreService {
     private firebase: AngularFirestore
   ) { }
 
-  create() {
-    this.firebase.collection('test').add({ test: true }).then(res => console.log(res));
+  create(data: any, path: string, id: string) {
+    const collection = this.firebase.collection(path);
+    return collection.doc(id).set(data);
   }
+
+  get(path: string, id: string) {
+    const collection = this.firebase.collection(path);
+    return collection.doc(id).valueChanges();
+  }
+
+  delete(path: string, id: string) {
+    const collection = this.firebase.collection(path);
+    return collection.doc(id).delete();
+  }
+
+  update(data: any, path: string, id: string) {
+    const collection = this.firebase.collection(path);
+    return collection.doc(id).update(data);
+  }
+
+  createId() {
+    return this.firebase.createId();
+  }
+
+  getCollection<type>(path: string) {
+    const collection = this.firebase.collection<type>(path);
+    return collection.valueChanges();
+  }
+
 }
